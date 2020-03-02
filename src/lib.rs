d9b46f912266242eeff7c1f7509a2ac805a5efeb
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2020-03-02 22:32:32 +0100

    Reexport mio::net::Unix*

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,9 @@
-//! MIO bindings for Unix Domain Sockets
+//! Deprecated MIO bindings for Unix Domain Sockets
 
 #![cfg(unix)]
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/mio-uds/0.6")]
+#![doc(html_root_url = "https://docs.rs/mio-uds/0.7")]
 
-extern crate iovec;
-extern crate libc;
 extern crate mio;
 
-use std::io;
-
-mod datagram;
-mod listener;
-mod socket;
-mod stream;
-
-pub use stream::UnixStream;
-pub use listener::UnixListener;
-pub use datagram::UnixDatagram;
-
-fn cvt(i: libc::c_int) -> io::Result<libc::c_int> {
-    if i == -1 {
-        Err(io::Error::last_os_error())
-    } else {
-        Ok(i)
-    }
-}
+pub use mio::net::{UnixStream, UnixListener, UnixDatagram};
